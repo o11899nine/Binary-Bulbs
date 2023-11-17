4dd6c1322fe5b7ebb931b6c12d76ed202e86550b
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-11-17 22:53:33 +0100

    Add toggle button to hide card

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const lightBulbs = document.querySelectorAll(".lightbulb");
+const toggleBtn = document.querySelectorAll(".hide-btn")
 
 
 lightBulbs.forEach(function (lightbulb) { 
-    lightbulb.addEventListener('click', switchBulb)
+    lightbulb.addEventListener('click', toggleLight)
 });
 
+toggleBtn.forEach(function (button) {
+    button.addEventListener('click', toggleCardVisibility)
+});
 
-function switchBulb() {
+function toggleCardVisibility() {
+    let button = event.target;
+    button.innerText = "+";
+    let card = button.parentElement;
+    let binaryValue = card.children[0];
+    let bulb = card.children[1];
+    binaryValue.style.visibility = "hidden";
+    bulb.style.visibility = "hidden";
+}
+    
+function toggleLight() {
     event.currentTarget.classList.toggle("on");
 }
 
