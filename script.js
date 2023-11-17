6a2ce6db52198b9f0c4ee1c7ba497b57b6aa3e31
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-11-17 23:36:37 +0100

    Add button to hide or show all

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 const lightBulbs = document.querySelectorAll(".lightbulb");
-const toggleBtn = document.querySelectorAll(".hide-btn")
+const cardVisibilityButtons = document.querySelectorAll(".card-visibility-btn")
+const allCardsVisibilityButton = document.querySelector(".all-cards-visibility-btn")
+const cards = document.querySelectorAll(".card")
 
 
 lightBulbs.forEach(function (lightbulb) { 
     lightbulb.addEventListener('click', toggleLight)
 });
 
-toggleBtn.forEach(function (button) {
-    button.addEventListener('click', toggleCardVisibility)
+cardVisibilityButtons.forEach(function (button) {
+    button.addEventListener('click', function() {
+        const card = this.parentElement;
+        toggleSingleCardVisibility(card);
+    });
 });
 
-function toggleCardVisibility() {
-    let button = event.target;
-    button.innerText = "+";
-    let card = button.parentElement;
+allCardsVisibilityButton.addEventListener('click', toggleAllCardsVisibility);
+
+function toggleSingleCardVisibility(card) {
     let binaryValue = card.children[0];
     let bulb = card.children[1];
-    binaryValue.style.visibility = "hidden";
-    bulb.style.visibility = "hidden";
+    let button = card.children[2];
+
+    if (button.innerText === "+") {
+    binaryValue.style.visibility = "visible";
+    bulb.style.visibility = "visible";
+    button.innerText = "X";
+        
+    } else {
+        binaryValue.style.visibility = "hidden";
+        bulb.style.visibility = "hidden";
+        button.innerText = "+";
+        if (bulb.classList.contains("on")) {
+            bulb.classList.toggle("on");
+        }
+    }
 }
     
 function toggleLight() {
     event.currentTarget.classList.toggle("on");
+    console.log(event.currentTarget.getAttribute("data-binaryValue"));
 }
 
+function toggleAllCardsVisibility() {
+    if (allCardsVisibilityButton.innerText === "Hide all") {
+        allCardsVisibilityButton.innerText = "Show all";
+    } else {
+        allCardsVisibilityButton.innerText = "Hide all";
+    }
+    cards.forEach(function (card) { 
+        toggleSingleCardVisibility(card);
+});
+}
